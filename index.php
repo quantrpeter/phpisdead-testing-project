2cb9fb429a1475052d47bd99ac666fe2339fd9de
Peter <peter@quantr.hk>
2024-11-19 01:25:57 +0800

    dev

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 
 require_once 'vendor/autoload.php';
 
-use \Quantr\PhpIsDead\Router;
+use \Quantr\PhpIsDead\PhpIsDead;
 
-$router=new Router();
-$router->add('/index', 'peter');
-echo $router->toString();
+// $router=new Router();
+// $router->add('/index', 'peter');
+// echo $router->toString();
+
+$phpIsDead = new PhpIsDead();
+$phpIsDead->start();
+
+$phpIsDead->router->add('/index', 'peter');
+echo $phpIsDead->router->toString();
+
+echo "<br>" . $_SERVER['REQUEST_URI'];
